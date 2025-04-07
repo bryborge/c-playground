73f42126dadecadb480c8f79a277d30c3242dc6d
Bryan Borgeson <bryborge@gmail.com>
2025-04-07 10:27:37 -0700

    Add design.txt and comment in the format the professor expects

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*******************************************************************************
+# Author(s):        <AUTHOR_NAME>
+# Lab:              <LAB_NAME/NUMBER>
+# Date:             <DATE>
+# Description:      <IN SIMPLE TERMS, DESCRIBE WHAT THIS PROGRAM DOES?>
+# Input:            <INPUTS/ARGS>
+# Output:           <OUTPUTS/RETURN VALUES>
+# Source(s):        <SOURCES>
+#******************************************************************************/
 int main(void) {
     printf("Hello, World!\n");
     return 0;
